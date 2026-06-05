dc655dddac8b8ccf0de390f23aa5714889ae4153
Anubhav Rai <iamraianubhav@gmail.com>
2026-06-05 23:17:03 +0530

    Add files via upload

---
+++ day_9first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter number of rows: ");
+    scanf("%d", &num);
+    for (int i = num; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
---
+++ day_9fourth.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number of rows: ");
+    scanf("%d", &num);
+    for (int i = 1; i <= num; i++)
+    {
+        for (int j = 1; j <=num; j++)
+        {
+            if (i == 1 || j == 1 || i == num || j == num)
+            {
+                printf("*");
+            }
+            else {
+                printf(" ");
+            }
+
+        }
+        printf("\n");
+    }
+}
