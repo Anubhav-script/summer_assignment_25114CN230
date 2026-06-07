bf785881c9d66fa3bb9a64c9a649092fe4651d34
Anubhav Rai <iamraianubhav@gmail.com>
2026-06-07 23:17:29 +0530

    Add files via upload

---
+++ day_11second.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int maximum(int a, int b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+int main()
+{
+    int x, y, result;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    result = maximum(x, y);
+    printf("Maximum = %d\n", result);
+
+    return 0;
+}
