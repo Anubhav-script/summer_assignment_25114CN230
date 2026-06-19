81403a7c6a57c6d02eeda743a0aed92a3fb1f45b
Anubhav Rai <iamraianubhav@gmail.com>
2026-06-19 21:39:16 +0530

    Add files via upload

---
+++ Day 22/day_22second.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[200];
+    int i, words = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ' || str[i] == '\n')
+        {
+            words++;
+        }
+    }
+
+    printf("Word count: %d\n", words);
+}
